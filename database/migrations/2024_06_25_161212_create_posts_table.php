49564ed8168f31144d85cd3b7c1dd7d73acd304a
stark3w <stark3w@gmail.com>
2024-06-25 23:42:10 +0700

    first commit

---
+++ database/migrations/2024_06_25_161212_create_posts_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->string('image_path')->nullable();
+            $table->bigInteger('likes')->default(0);
+            $table->bigInteger('views')->default(0);
+            $table->boolean('is_published')->default(true);
+            $table->timestamps();
+
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->index('category_id', 'post_category_idx');
+            $table->foreign('category_id', 'post_category_fk')->references('id')->on('categories');
+
+            $table->unsignedBigInteger('tag_id')->nullable();
+            $table->index('tag_id', 'post_tag_idx');
+            $table->foreign('tag_id', 'post_tag_fk')->references('id')->on('tags');
+
+
+
+
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title','Главная страница')
+
+@section('content')
+    <div class="container d-flex justify-content-center align-items-center"><h1>Добро пожаловать на CodeCraze BLOG</h1></div>
+@endsection
